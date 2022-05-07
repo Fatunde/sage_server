f1239f8a8ea6056c516b9381a25e68692de4bf9c
Damilare <fatundedamilare@gmail.com>
2022-05-07 20:12:52 +0100

    first commit

--- config/development.js
+++ config/development.js
@@ -1,4 +1,4 @@
 module.exports = { 
-    db_string : process.env.DB_STRING || "kjdsmcieroieiore",
+    db_string : process.env.DB_STRING || "mongodb+srv://Damilare:Damilare@cluster0.ignbg.mongodb.net/sageBank?retryWrites=true&w=majority",
     jwt_secret : process.env.JWT_SECRET || "umbrella_dev"
 }
